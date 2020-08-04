1a8b276330c933d57b86297e7b8573e6eac90bc5
taqnar <muzammilthottathil.official@gmail.com>
2020-08-04 17:33:26 +0530

    Updates

---
+++ precache-manifest.1645bc8f88e9b379b81d9d4237d4a4f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "101ab6414999883e6c986592806ef82c",
+    "url": "/nss-BloodCell/index.html"
+  },
+  {
+    "revision": "c5fd63efda2a8ecbeb9f",
+    "url": "/nss-BloodCell/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7811c792add3e73ce6b5",
+    "url": "/nss-BloodCell/static/js/2.5c471fdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nss-BloodCell/static/js/2.5c471fdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5fd63efda2a8ecbeb9f",
+    "url": "/nss-BloodCell/static/js/main.01866da9.chunk.js"
+  },
+  {
+    "revision": "bdfca6b5f185ef5feb59",
+    "url": "/nss-BloodCell/static/js/runtime-main.83668464.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nss-BloodCell/static/media/logo.5d5d9eef.svg"
+  }
+]);
